328c1150017a5fcdd330a2c9c7e4192f15833360
Satvik Vansh (BIT) <satvikvansh@gmail.com>
2026-06-01 18:43:21 +0530

    Spring Boot Setup

---
+++ backend/src/main/java/com/project/ai_interview_backend/AiInterviewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.ai_interview_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiInterviewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiInterviewBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/ai_interview_backend/AiInterviewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ai_interview_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiInterviewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
